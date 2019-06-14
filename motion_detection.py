521f0e80704e3e74061588cb75661f40f55323aa
satish347 <venkata.satish@shrisinfotech.com>
2019-06-14 17:31:32 +0530

    code changed

--- motion_detection.py
+++ motion_detection.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 
 cap=cv2.VideoCapture(0)
-cap.set(3,1000)
-cap.set(4,1200)
 background_sub=cv2.createBackgroundSubtractorMOG2(10,400,True)
 frame_count=0
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 24.0, (640,480))
 while (cap.isOpened()):
 	ret,frame=cap.read()
 	if ret==True:
@@ -14,9 +14,10 @@ while (cap.isOpened()):
 		count=np.count_nonzero(bg_mask)
 		#print(count)
 		if (frame_count > 1 and count > 1000):
-			cv2.putText(frame, 'motion detected', (650, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+			cv2.putText(frame, 'motion detected', (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 			
 		cv2.imshow('Frame', frame)
+		out.write(frame)
 	if cv2.waitKey(1) & 0xFF==ord('q'):
 		break;
 cap.release()
